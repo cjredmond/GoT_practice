620b424c569528c9ef7359e4c0ddde4fed65a333
cjredmond <connor.redmond@gmail.com>
2017-05-09 11:58:53 -0400

    added views for house list and detail

--- houses/models.py
+++ houses/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 class House(models.Model):
     name = models.CharField(null=True, max_length=40)
-    motto = models.CharField(null=True, max_length=50)
+    motto = models.CharField(null=True, max_length=50, blank=True)
     sigil = models.FileField()
-    region = models.CharField()
+    region = models.CharField(max_length=40)
     major = models.BooleanField()
 
     @property
     def image_url(self):
         return self.sigil.url
+
+    def __str__(self):
+        return self.name
--- houses/views.py
+++ houses/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from houses.models import House
 
-# Create your views here.
+class MajorHouseListView(ListView):
+    model = House
+
+    queryset = House.objects.filter(major=True)
+    context_object_name = 'houses'
+
+
+class HouseDetailView(DetailView):
+    model = House
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        target = House.objects.get(id=self.kwargs['pk'])
+        context['minor'] = House.objects.filter(major=False,region=target.region)
+        context['house'] = target
+        print(target.major)
+        return context
